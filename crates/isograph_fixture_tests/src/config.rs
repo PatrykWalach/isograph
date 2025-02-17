4784a0f43c3c4499f732bac1b2b9ba85b1242668
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-02-17 23:24:06 +0100

    run fixtures on windows

--- crates/isograph_fixture_tests/src/config.rs
+++ crates/isograph_fixture_tests/src/config.rs
@@ -4,18 +4,34 @@ use intern::string_key::Intern;
 use isograph_config::{absolute_and_relative_paths, CompilerConfig};
 
 pub fn isograph_config_for_tests(current_working_directory: &Path) -> CompilerConfig {
+    let mut path_root = "".to_string();
+
+    if cfg!(windows) {
+        // handle different disks C:\
+        path_root.push_str(
+            PathBuf::from(current_working_directory)
+                .ancestors()
+                .last()
+                .expect("Expected root path")
+                .to_str()
+                .expect("Expected root path"),
+        )
+    } else {
+        path_root.push('/')
+    };
+
     let current_working_directory = current_working_directory.to_str().unwrap().intern().into();
 
     CompilerConfig {
-        config_location: PathBuf::from("/test-config-location"),
-        project_root: PathBuf::from("/test-project-root"),
+        config_location: ([&path_root, "test-config-location"].iter().collect()),
+        project_root: ([&path_root, "test-project-root"].iter().collect()),
         artifact_directory: absolute_and_relative_paths(
             current_working_directory,
-            PathBuf::from("/test-artifact-directory"),
+            [&path_root, "test-artifact-directory"].iter().collect(),
         ),
         schema: absolute_and_relative_paths(
             current_working_directory,
-            PathBuf::from("/test-schema"),
+            [&path_root, "test-schema"].iter().collect(),
         ),
         schema_extensions: vec![],
         options: Default::default(),
